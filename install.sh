a8403f7bc17c303224d06d463bc37b12cdfc84f6
Cem Kağan <cemkagankayili@hotmail.com>
2022-02-09 16:55:33 +0300

    added alacritty and starship config

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mkdir -pv ${HOME}/.config/alacritty
+cp -v config_files/alacritty.yml ${HOME}/.config/alacritty/alacritty.yml 
+cp -v config_files/starship.toml ${HOME}/.config/starship.toml
+
+
